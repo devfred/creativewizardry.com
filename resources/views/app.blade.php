1261e56c2ee8966c7c0ca4d2e3ef33f798e1f5b9
Frederick King <mcdevfred@gmail.com>
2015-03-23 16:03:07 -0400

    Cleanup app.blade.php master file and initial views.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,41 +1,35 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Laravel</title>
-
-	<link href="{{ asset('/css/app.css') }}" rel="stylesheet">
-
+	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<title>Creative Wizardry</title>
+	<meta property="og:title" content="Creative Wizardry" />
+    <meta property="og:site_name" content="http://creativewizardry.com"/>
+    <meta property="og:url" content="http://creativewizardry.com"/>
+    <meta property="og:description" content="Creative Wizardry"/>
+	<!-- Stylesheets -->
+	<link href="{{ asset('/css/app.css') }}" rel="stylesheet"/>
 	<!-- Fonts -->
-	<link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
-
-	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-	<!--[if lt IE 9]>
-		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-	<![endif]-->
+	<link href="//fonts.googleapis.com/css?family=Roboto:400,300" rel="stylesheet" type="text/css"/>
 </head>
 <body>
-	<nav class="navbar navbar-default">
-		<div class="container-fluid">
-			<div class="navbar-header">
+	<div id="page" class="page-width">
+		<div id="page-nav">
+			<nav>
 				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
 					<span class="sr-only">Toggle Navigation</span>
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 				</button>
-				<a class="navbar-brand" href="#">Laravel</a>
-			</div>
 
-			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 				<ul class="nav navbar-nav">
 					<li><a href="{{ url('/') }}">Home</a></li>
+					<li><a href="{{ url('/about') }}">About</a></li>
+					<li><a href="{{ url('/contact') }}">Contact</a></li>
 				</ul>
-
 				<ul class="nav navbar-nav navbar-right">
 					@if (Auth::guest())
 						<li><a href="{{ url('/auth/login') }}">Login</a></li>
@@ -49,12 +43,23 @@
 						</li>
 					@endif
 				</ul>
-			</div>
+			</nav>
+			<div class="clear"></div>
 		</div>
-	</nav>
-
-	@yield('content')
-
+		<div id="page-head"></div>
+		<div id="page-body">
+			<div id="body-primary" class="page-col">
+				@yield('content')
+			</div>
+			<div id="body-secondary" class="page-col">
+				@yield('secondary')
+			</div>
+			<div class="clear"></div>			
+		</div>
+		<div id="page-foot">
+			<p>Disclaimer: The opinions expressed are my own personal opinions and do not represent my employer's view in any way.<p>
+		</div>
+	</div>
 	<!-- Scripts -->
 	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 	<script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
