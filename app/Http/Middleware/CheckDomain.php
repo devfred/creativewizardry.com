89e587d945c82b6e4fd0e2abf4be38c80a7471fc
Frederick King <mcdevfred@gmail.com>
2015-07-28 02:17:30 -0400

    Added CheckDomain middleware

---
+++ app/Http/Middleware/CheckDomain.php
@@ -0,0 +1,25 @@
+<?php namespace App\Http\Middleware;
+
+use Closure;
+
+class CheckDomain {
+
+	/**
+	 * Handle an incoming request.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  \Closure  $next
+	 * @return mixed
+	 */
+	public function handle($request, Closure $next)
+	{
+		
+		if( $request->server->get('HTTP_HOST') != getenv('SITE_URL') )
+		{
+			\Redirect::to( getenv('SITE_URL') );
+		}
+		
+		return $next($request);
+	}
+
+}
