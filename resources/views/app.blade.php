36eb6eef198ea709d3a54f48c3dd96f2b536f6c0
DevFred <mcdevfred@gmail.com>
2025-03-22 18:31:08 -0700

    Update views

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -18,9 +18,9 @@
         @endif
 
         @yield('header_scripts')
-        
+
     </head>
-    <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
+    <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col text-white text-left">
         
         @yield('content')        
         
--- resources/views/content/content.blade.php
+++ resources/views/content/content.blade.php
@@ -1,25 +1,25 @@
 @extends('app')
 @section('content')
+    <div class="articles">
     @foreach($contentItems as $item)    
     @if ( strtotime($item->published_at) <= strtotime('now') )
-    <div class="articles">
-        <article>
-            <div class="article">
-                <p class="category"><a href="{{ url("/categories/" . $item->category->slug) }}">{{$item->category->name}}</a></p>
-                <h2><a href="{{ url( '/'. $item->slug ) }}">{!!$item->title!!}</a></h2>
-                <p class="meta">
-                    <i class="fa fa-tags"></i>
-                    @foreach($item->tags as $tag)
-                        <span class="tag "><a class="label label-default" href="{{ url("/tags/" . $tag->slug) }}">{{$tag->name}}</a> </span>
-                    @endforeach
-                    <span class="time"><i class="fa fa-calendar"></i> <time>{{ date("F d, Y",strtotime($item->published_at)) }}</time></span>
-                </p>
-                <p>{!! $item->excerpt !!}</p>
-                <a href="{{ url( '/'. $item->slug ) }}">Read More</a>
-            </div>
-        </article>
-    </div><!-- end .articles -->
+            
+        <div class="article mb-2">
+            <p class="category"><a href="{{ url("/categories/" . $item->category->slug) }}">{{$item->category->name}}</a></p>
+            <h2><a href="{{ url( '/'. $item->slug ) }}">{!!$item->title!!}</a></h2>
+            <p class="meta">
+                <i class="fa fa-tags"></i>
+                @foreach($item->tags as $tag)
+                    <span class="tag "><a class="label label-default" href="{{ url("/tags/" . $tag->slug) }}">{{$tag->name}}</a> </span>
+                @endforeach
+                <span class="time"><i class="fa fa-calendar"></i> <time>{{ date("F d, Y",strtotime($item->published_at)) }}</time></span>
+            </p>
+            <p>{!! $item->excerpt !!}</p>
+            <a href="{{ url( '/'. $item->slug ) }}">Read More</a>
+        </div>        
+    
     @endif
     @endforeach
-    {!! $contentItems->render() !!}
+    </div><!-- end .articles -->
+    
 @endsection
