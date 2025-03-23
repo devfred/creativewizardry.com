90b62537e8f64d7b37c93d1a082749133500385d
DevFred <mcdevfred@gmail.com>
2025-03-23 08:45:06 -0700

    Create about.blade.php

---
+++ resources/views/content/about.blade.php
@@ -0,0 +1,29 @@
+@extends('app')
+@section('content')
+<div class="bg-white py-24 sm:py-32">
+  <div class="mx-auto max-w-7xl px-6 lg:px-8">
+    <div class="mx-auto max-w-2xl lg:mx-0">
+      <h2 class="text-4xl font-semibold tracking-tight text-pretty text-gray-900 sm:text-5xl"><a href="/">Creative Wizardry</a></h2>
+      <p class="mt-2 text-lg/8 text-gray-600">Notes / Thoughts / Research</p>
+    </div>
+    <div class="mx-auto mt-10 max-w-2xl border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none text-gray-900">
+    <h2 class="mt-3 text-lg/6 font-semibold text-gray-900 group-hover:text-gray-600">About Me</h2>    	
+    <p>My name is Fred, and I'm a full-stack web developer with expertise in crafting user-centric front-end experiences using HTML, CSS, and JavaScript. On the back end, I have proficiency in both .NET and PHP frameworks.</p>
+    <p>I specialize in API development and integrations with third-party APIs, enabling robust data exchange and functionality within applications. My homelab experience has provided me with hands-on knowledge in infrastructure management and automation, ensuring efficient and reliable operations.</p>
+    <p>Whether tackling complex challenges or innovating new solutions, my goal is to solve users' problems creatively, delivering applications that exceed their expectations.</p>    
+    <div class="module social">
+        <h4>On the Interweb</h4>        
+        <a href="https://x.com/CWizardry" target="_blank" class="strike-tooltip tw" title="Visit Twitter">Twitter</a>
+        <a href="https://www.youtube.com/channel/UCYFGkFIPWbco6XkFHYyTscw" target="_blank" class="strike-tooltip yt" title="Visit YouTube">YouTube</a>
+        <a href="https://plus.google.com/u/0/100744637735711715607/posts" target="_blank" class="strike-tooltip gp" title="Visit Google Plus"><i class="fa fa-google-plus"></i></a>
+        <a href="https://github.com/devfred" target="_blank" class="strike-tooltip gh" title="Visit Github"><i class="fa fa-github"></i></a>
+        <a href="https://gist.github.com/devfred" target="_blank" class="strike-tooltip gh" title="Visit Gists"><i class="fa fa-code-fork"></i></a>
+        <a href="https://www.linkedin.com/pub/fred-king/34/b16/9" target="_blank" class="strike-tooltip li" title="Visit LinkedIn"><i class="fa fa-linkedin"></i></a>
+
+    </div>
+
+             
+    </div>
+  </div>
+</div>    
+@endsection
