dcf681f1362f91e69504db10a9703c05db483ed6
DevFred <mcdevfred@gmail.com>
2025-03-22 18:52:23 -0700

    Cleanup routes. Remove unused controllers for now. Moved primary function from deprecated controllers to content controller.

--- app/Http/Controllers/ContentController.php
+++ app/Http/Controllers/ContentController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use App\Models\ContentItem;
+use App\Models\Tag;
+use App\Models\Category;
 
 use Illuminate\View\View;
 
@@ -15,22 +17,28 @@ class ContentController extends Controller
 		return view('content.content', compact('contentItems'));
 	}
 
-	public function detail(string $slug): view
+	public function get_content_by_slug(string $slug): view
 	{	
         $contentItems = ContentItem::where('is_published', true)->orderBy('published_at', 'DESC')->paginate(5);
 		return view('content.content', compact('contentItems'));
 	}
 
-	public function get_content_by_tag(string $tag): view
+	public function get_content_by_tag(string $tagSlug): view
 	{	
-        $contentItems = ContentItem::where('is_published', true)->orderBy('published_at', 'DESC')->paginate(5);
-		return view('content.content', compact('contentItems'));
+		$tag = Tag::findBySlug($tagSlug);
+        $contentItems = ContentItem::with('tags', 'category')->whereHas('tags', function ($query) use ($tagSlug) {
+            $query->whereSlug($tagSlug);
+        })->where('is_published', true)->orderBy('published_at', 'DESC')->paginate(5);        
+        return view('content.content', compact('contentItems', 'tag') );       
 	}
 
-	public function get_content_by_category(string $category): view
+	public function get_content_by_category(string $categorySlug): view
 	{	
-        $contentItems = ContentItem::where('is_published', true)->orderBy('published_at', 'DESC')->paginate(5);
-		return view('content.content', compact('contentItems'));
+        $category = Category::findBySlug($categorySlug);
+        $contentItems = ContentItem::with('tags', 'category')->whereHas('category', function ($query) use ($categorySlug) {
+            $query->whereSlug($categorySlug);
+        })->where('is_published', true)->orderBy('published_at', 'DESC')->paginate(5);
+        return view('content.content', compact('contentItems', 'category'));
 	}
 
 
--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,9 @@ use App\Http\Controllers\SearchController;
 
 Route::get('/', [ContentController::class, 'index']);
 Route::get('about',[ContentController::class, 'index']);
-Route::get('/{slug}',[ContentController::class, 'detail']);
-Route::get('tags/{tag}',[ContentController::class, 'get_content_by_tag']);
-Route::get('categories/{category}',[ContentController::class, 'get_content_by_category']);
+Route::get('/{slug}',[ContentController::class, 'get_content_by_slug']);
+Route::get('tags/{tagSlug}',[ContentController::class, 'get_content_by_tag']);
+Route::get('categories/{categorySlug}',[ContentController::class, 'get_content_by_category']);
 Route::get('search/{term}', [SearchController::class, 'search']);
 Route::post('search', function() {
     $term = Input::get('term');
